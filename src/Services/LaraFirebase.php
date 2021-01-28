2c9dafab0c9541d0a65b904310615ef260a9fdf4
Gentrit Abazi <gentritabazi01@gmail.com>
2021-01-28 23:31:19 +0100

    Init

---
+++ src/Services/LaraFirebase.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace KutiaDigitalAgency\Larafirebase\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class Firebase
+{
+    private $title;
+    
+    private $body;
+
+    private $clickAction;
+
+    private $image;
+
+    private $fromArray;
+
+    const FIREBASE_SEND_NOTIFICATION_URL = 'https://fcm.googleapis.com/fcm/send';
+
+    public function withTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function withBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function withClickAction($clickAction)
+    {
+        $this->clickAction = $clickAction;
+
+        return $this;
+    }
+
+    public function withImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function fromArray($fromArray)
+    {
+        $this->fromArray = $fromArray;
+
+        return $this;
+    }
+
+    public function sendNotification($tokens)
+    {
+        $fields = array(
+            'registration_ids' => $tokens,
+            'notification' => ($this->fromArray) ? $this->fromArray : [
+                'title' => $this->title,
+                'body' => $this->body,
+                'image' => $this->image,
+                'click_action' => $this->clickAction
+            ],
+            'data' => ['Nick' => 'a']
+        );
+        
+        return $this->callApi($fields);
+    }
+
+    public function sendMessage($tokens)
+    {
+        $fields = array(
+            'registration_ids' => $tokens,
+            'data' => ($this->fromArray) ? $this->fromArray : [
+                'title' => $this->title,
+                'body' => $this->body,
+                'image' => $this->image
+            ]
+        );
+        
+        return $this->callApi($fields);
+    }
+
+    public function callApi($fields)
+    {
+        $response = Http::withHeaders([
+            'Authorization' => 'key='. config('larafirebase.authentication_key')
+        ])->post(self::FIREBASE_SEND_NOTIFICATION_URL, $fields);
+
+        return $response->body();
+    }
+}
